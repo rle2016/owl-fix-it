4ff0abf5cfd9e073c4db82c2071ec70b7a6b691a
rle2016 <rle2016@fau.edu>
2016-11-02 12:15:45 -0400

    Update Form.php
    removed image filters from the form

--- Project_Owl/form.php
+++ Project_Owl/form.php
@@ -33,19 +33,13 @@ if (isset($_SESSION['username_input']))
     <title>Project 5</title>
     
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-    
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     
     <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body onload="initialize();">
-<!-- Navigation -->
+    <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -56,28 +50,22 @@ if (isset($_SESSION['username_input']))
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
+                <a href="index.php"><img src="images/owl-logo.png" class="img-responsive" height="260" width="160" alt=""></a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="wall.php">Wall</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="logout.php">Logout</a>
-                    </li>
+                    <li class="hidden"><a href="#page-top"></a></li>
+                    <li><a class="page-scroll" href="wall.php">Home</a></li>
+                    <li><a class="page-scroll" href="form.php">Upload Photo</a></li>
+                    <li><a class="page-scroll" href="logout.php">Logout</a></li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
+            </div>
+            <!-- /.container-fluid -->
     </nav>
-    <hr>
-    <br>
     <div class="container">    
         <div class="row">
             <div id="formParent" class="col-md-6 col-md-offset-3 text-center">
@@ -103,28 +91,14 @@ if (isset($_SESSION['username_input']))
                     
                     <div class="form-group">
                         <label class="sr-only" for="image">Original Image</label>
-                        <img id="image" name="image" src="img/default.png" width="50%">
+                        <img id="image" name="image" src="images/default.png" width="50%">
                     </div>
                     <br>
-                        <input type="file" id="upload" name="upload" accept="image/*" required="true" class="filestyle" data-buttonName="btn-primary" data-buttonBefore="true">
-                    <div class="form-group">
-                        <h3>Filter Photo</h3>
-                        <div class="checkbox-inline">
-                            <label for="myNostalgia">My Nostalgia</label>
-                            <input type="radio" name="filter" id="myNostalgia" value="myNostalgia" onclick="applyMyNostalgiaFilter();" required="true">
-                        </div>
-                        <div class="checkbox-inline">
-                            <label for="grayscale">Grayscale</label>
-                            <input type="radio" name="filter" id="grayscale" value="grayscale" onclick="applyGrayscaleFilter();" required="true">
-                        </div>
-                        <div class="checkbox-inline">
-                            <label for="original">Original</label>
-                            <input type="radio" name="filter" id="lomo" value="lomo" onclick="revertToOriginal();">
-                        </div>
-                    </div>       
-                    <div class="form-group">         
-                    <input type="submit" value="Upload image to wall!" class="btn btn-primary col-md-offset-1" required="true">
-                    <input type="button" id="resetForm" value="Start over!" class="btn btn-default">
+                    <input type="file" id="upload" name="upload" accept="image/*" required="true" class="filestyle" data-buttonName="btn-primary" data-buttonBefore="true"> 
+                    <hr>
+                    <div id="buttonPost" class="form-group">         
+                    <input type="submit" value="Upload Post" class="btn btn-primary col-md-offset-1" required="true">
+                    <input type="button" id="resetForm" value="Start over" class="btn btn-default">
                     </div>
                 </form>
             </div>
@@ -133,13 +107,13 @@ if (isset($_SESSION['username_input']))
 
     <!-- Footer -->
     <div id="footer">
-    <footer class="text-center">
-                <div class="row">
-                    <div class="col-lg-12">
-                        Copyright &copy; Ryan Le 2016
-                    </div>
+        <footer class="text-center">
+            <div class="row">
+                <div class="col-lg-12">
+                    Copyright &copy; 2016 Team Petabyte
                 </div>
-    </footer>
+            </div>
+        </footer>
     </div>
     <!-- JavaScript placed at bottom for faster page loadtimes. -->
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
